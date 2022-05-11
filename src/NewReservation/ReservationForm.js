7fb07fd3f2a158f704fe7ec9b22fa1dc255b7c90
mturczak <mturczak@users.noreply.github.com>
2022-05-12 00:38:04 +0200

    css

--- src/NewReservation/ReservationForm.js
+++ src/NewReservation/ReservationForm.js
@@ -6,19 +6,19 @@ const ReservationForm = () => {
     <form className="formclass" >
         <div className="new-reservation__controls">
       
-      <div className="new_reservation__control">
+      <div className="new-reservation__control">
         <label>Stolik</label>
         <input type="text" />
       </div>
-      <div className="new_reservation__control">
+      <div className="new-reservation__control">
         <label>Ilość osób</label>
         <input type="number" />
       </div>
-      <div className="new_reservation__control">
+      <div className="new-reservation__control">
         <label>Data</label>
         <input type="date" />
       </div>
-      <div className="new_reservation__control">
+      <div className="new-reservation__control">
         <label>Godzina</label>
         <input type="time" />
       </div>
