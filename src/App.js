c2671a376ba0de64c2b2761e0d78368fc40bc73c
mturczak <mturczak@users.noreply.github.com>
2022-07-31 01:27:48 +0200

    sql backend

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import NavBar from "./NavBar/NavBar";
 import ReservationForm from "./NewReservation/ReservationForm";
 import ReservationPreview from "./NewReservation/ReservationPreviev";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 const example_tables = [
   { id: 1, table_size: 1, isBusy: false },
@@ -14,38 +15,91 @@ const example_tables = [
   { id: 6, table_size: 4, isBusy: true },
 ];
 
-const example_reservations = [
-  {
-    id: 1,
-    table_id: 1,
-    size: 1,
-    reservation_date: "2022-08-10",
-    reservation_time: "15:00",
-  },
-  {
-    id: 2,
-    table_id: 2,
-    size: 2,
-    reservation_date: "2022-08-11",
-    reservation_time: "16:00",
-  },
-  {
-    id: 3,
-    table_id: 3,
-    size: 2,
-    reservation_date: "2022-08-12",
-    reservation_time: "17:00",
-  },
-];
+// const example_reservations = [
+//   {
+//     id: 1,
+//     table_id: 1,
+//     size: 1,
+//     reservation_date: "2022-08-10",
+//     reservation_time: "15:00",
+//   },
+//   {
+//     id: 2,
+//     table_id: 2,
+//     size: 2,
+//     reservation_date: "2022-08-11",
+//     reservation_time: "16:00",
+//   },
+//   {
+//     id: 3,
+//     table_id: 3,
+//     size: 2,
+//     reservation_date: "2022-08-12",
+//     reservation_time: "17:00",
+//   },
+// ];
+
+
+
+
 
 const App = (props) => {
   const [reservations, setReservations] = useState(example_reservations);
-  const addReservationHandler = (reservation) => {
-    setReservations((prevReservations) => {
-      console.log([...prevReservations, reservation]);
-      return [...prevReservations, reservation];
+  const [example_tables_2, setExampleTables2] = useState([]);
+
+  useEffect(() => {
+    const fetchTables = async () => {
+      const response = await fetch("/reservation/tables");
+      const json = await response.json();
+      console.log(json);
+      if (response.ok) {
+        console.log("ok");
+       setExampleTables2(json);
+        console.log(json);
+        console.log("example tables 2", example_tables_2);
+      }
+    };
+
+    fetchTables();
+  }, []);
+
+  // const addReservationHandler2 = (reservation) => {
+  //   setReservations((prevReservations) => {
+  //     console.log([...prevReservations, reservation]);
+  //     return [...prevReservations, reservation];
+  //   });
+  // };
+
+  const addReservationHandler = async (reservation) => {
+    const response = await fetch("/reservation", {
+      method: "POST",
+      body: JSON.stringify(reservation),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-  };
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      console.log(json.error)
+      
+      // setError(json.error);
+      // setEmptyFields(json.emptyFields);
+    }
+    if (response.ok) {
+      // setTitle("");
+      // setLoad("");
+      // setReps("");
+      // setError(null);
+      // setEmptyFields([]);
+      console.log("new reservation added", json);
+      // dispatch({ type: "CREATE_WORKOUT", payload: json });
+
+  }
+}
+
+
   const onSaveReservationDataHandler = (enterReservationData) => {
     const reservationData = {
       ...enterReservationData,
@@ -61,7 +115,7 @@ const App = (props) => {
       <NavBar />
       <ReservationForm
         records={reservations}
-        tables={example_tables}
+        tables={example_tables_2}
         onSaveReservationData={onSaveReservationDataHandler}
         onSaveReservation={addReservationHandler}
       />
